bce64a62baabc7a19fa7ef94728a661ebe7056da
Vladimir Lidzhigoryaev <vlidzhigoryaev@digital-spirit.ru>
2025-03-18 14:49:17 +0300

    add solution

--- src/main/java/org/itmo/MagicCalendar.java
+++ src/main/java/org/itmo/MagicCalendar.java
@@ -1,13 +1,14 @@
 package org.itmo;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MagicCalendar {
     // Перечисление типов встреч
     public enum MeetingType {
         WORK, PERSONAL
     }
+
+    private final Map<Map.Entry<Integer, String>, MeetingType> meetings = new HashMap<>();
 
     /**
      * Запланировать встречу для пользователя.
@@ -21,7 +22,41 @@ public class MagicCalendar {
      */
     public boolean scheduleMeeting(String user, String time, MeetingType type) {
         // Реализация метода
-        return true;
+
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int minutes = Integer.parseInt(time.substring(3, 5));
+
+
+        if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) {
+            int sumMinutes = hours * 60 + minutes;
+
+            var userMeetings = meetings.keySet().stream().filter(k -> k.getValue().equals(user)).count();
+
+            if (userMeetings <= 5) {
+                if (meetings.keySet().stream().anyMatch(m ->
+                        m.getKey() >= sumMinutes && m.getKey() < (sumMinutes + 60) && m.getValue().equals(user))) {
+
+                    var timeInRange = meetings.keySet().stream().filter(
+                            m -> m.getKey() >= sumMinutes && m.getKey() < (sumMinutes + 60) && m.getValue().equals(user)
+                    ).findFirst().get();
+
+                    var meeting = meetings.get(timeInRange);
+
+                    if (type == MeetingType.PERSONAL && meeting.equals(MeetingType.WORK)) {
+                        meetings.remove(timeInRange);
+                        meetings.put(Map.entry(sumMinutes, user), type);
+                    } else {
+                        return false;
+                    }
+                } else {
+                    meetings.put(Map.entry(sumMinutes, user), type);
+                }
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -32,7 +67,7 @@ public class MagicCalendar {
      */
     public List<String> getMeetings(String user) {
         // Реализация метода
-        return new ArrayList<>();
+        return meetings.keySet().stream().filter(k -> k.getValue().equals(user)).map(Map.Entry::getValue).toList();
     }
 
     /**
@@ -46,6 +81,30 @@ public class MagicCalendar {
      */
     public boolean cancelMeeting(String user, String time) {
         // Реализация метода
+
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int minutes = Integer.parseInt(time.substring(3, 5));
+
+        if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) {
+            int sumMinutes = hours * 60 + minutes;
+
+            if (meetings.keySet().stream().anyMatch(m ->
+                    m.getKey() >= sumMinutes && m.getKey() < (sumMinutes + 60) && m.getValue().equals(user))) {
+
+                var timeInRange = meetings.keySet().stream().filter(
+                        m -> m.getKey() >= sumMinutes && m.getKey() < (sumMinutes + 60) && m.getValue().equals(user)
+                ).findFirst().get();
+
+                var meeting = meetings.get(timeInRange);
+
+                if (meeting.equals(MeetingType.WORK)) {
+                    meetings.remove(timeInRange);
+                    return true;
+                }
+            }
+
+        }
+
         return false;
     }
 }
